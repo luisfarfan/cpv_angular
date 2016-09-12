a4aae19f05148165337ebd9bb9a548310c159e56
Luis Eduardo Farfan Melgar <lucho.farfan9@gmail.com>
2016-09-12 15:02:43 -0500

    localStorage MyRouterLink and routing

--- app/core/MyRouterLink.ts
+++ app/core/MyRouterLink.ts
@@ -13,12 +13,15 @@ import {
 import {
     MenuServices
 } from './services/menu.service';
+import {
+    LoginService
+} from './services/login/login.service';
 
 @Component({
     selector: 'sidebar',
     template: `<ul #mymenu class="navigation navigation-main navigation-accordion"></ul>`,
     directives: [ROUTER_DIRECTIVES],
-    providers: [MenuServices]
+    providers: [MenuServices,LoginService]
 })
 
 export class MyRouterLink {
@@ -27,19 +30,34 @@ export class MyRouterLink {
     }) target: ViewContainerRef;
     private cmpRef: ComponentRef < any > ;
 
-    constructor(private dataServices: MenuServices, private compiler: Compiler, private _viewContainerRef: ViewContainerRef) {}
+    constructor(private dataServices: MenuServices, private compiler: Compiler, private _viewContainerRef: ViewContainerRef, private loginservice: LoginService) {}
+
+    getIdUsuario() {
+        let session = this.loginservice.getJsonSession();
+        console.log(session[0]['id_usuario'])
+        return session[0]['id_usuario'] || "";
+    }
 
     ngAfterViewInit() {
         if (this.cmpRef) {
             this.cmpRef.destroy();
         }
-        this.dataServices.getMenuLinks('?id_usuario=9').subscribe((res) => {
-            localStorage.setItem('menu', JSON.stringify(res))
-            let menustring = localStorage.getItem('menu')
-            this.compileToComponent(res).then((factory: ComponentFactory < any > ) => {
+        let id_usuario = this.getIdUsuario();
+        let queryparameter = `?id_usuario=${id_usuario}`;
+        if (id_usuario != "") {
+            this.dataServices.getMenuLinks(queryparameter).subscribe((res) => {
+                localStorage.setItem('menu', JSON.stringify(res))
+                let menustring = localStorage.getItem('menu')
+                this.compileToComponent(res).then((factory: ComponentFactory < any > ) => {
+                    this.cmpRef = this.target.createComponent(factory)
+                })
+            })
+        } else {
+            this.compileToComponent("").then((factory: ComponentFactory < any > ) => {
                 this.cmpRef = this.target.createComponent(factory)
             })
-        })
+        }
+
     }
 
     ngOnDestroy() {
